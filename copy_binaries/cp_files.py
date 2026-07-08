b3c4c6727b2e9873f542e78193004ec92a347215
ysrepo <slvkshkh@gmail.com>
2026-07-08 11:08:56 +0000

    normalize util py script

--- copy_binaries/cp_files.py
+++ copy_binaries/cp_files.py
@@ -1,16 +1,23 @@
+#!/usr/bin/env python3
+
 import os
 import shutil
 
-ffmpeg_str = "C:\\Users\\User\\Documents\\sandbox\\FFmpeg\\"
-current_loc = "C:\\Users\\User\\Documents\\cp_build\\MSYS2\\MinGW64\\"
+from pathlib import Path
+
+ffmpeg_str =  str(Path.cwd().parent / "FFmpeg")
+target_loc = str(Path.cwd() / "cp_build")
+
+print(ffmpeg_str)
+print(target_loc)
 
 with open("ffmpeg locations.txt") as textfile1, open("mingw64 locations.txt") as textfile2: 
     for x, y in zip(textfile1, textfile2):
-        x = x.strip()[2:].replace('/', '\\')
+        x = x.strip()[1:] #.replace('/', '\\')
         f = open(ffmpeg_str+x, 'r')
-        y = y.strip()[2:].replace('/', '\\')
-        if not os.path.exists(current_loc+y):
+        y = y.strip()[1:] #.replace('/', '\\')
+        if not os.path.exists(target_loc+y):
             # if the directory is not present 
             # then create it.
-            os.makedirs(current_loc+y)
-        shutil.copy(ffmpeg_str+x, current_loc+y)
+            os.makedirs(target_loc+y)
+        shutil.copy(ffmpeg_str+x, target_loc+y)
